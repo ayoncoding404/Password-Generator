a3eba9ab54482b0332ff17673a42df37e0ce17d1
ayoncoding404 <muhayon.rxz@gmail.com>
2025-01-27 03:21:54 +0600

    create input

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,30 @@ function App() {
     if (numAllowed) string += "0123456789";
     if (char) string += "!@#$%^&*_~";
 
-    for (let i = 1; i <= Array.length; i++) {
+    for (let i = 1; i <= array.length; i++) {
       let char = Math.floor(Math.random() * string.length + 1);
-      password = string.charAt(char)
+      password = string.charAt(char);
     }
- 
-     setPass(password);
 
+    setPass(password);
   }, [length, numAllowed, char, setPass]);
   return (
-    <div>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800">
       <h1 className="text-white text-4xl text-center">Password Generator</h1>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">test</div>
+      <div
+        className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-white
+      bg-gray-700 text-center"
+      >
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={pass}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+        </div>
+      </div>
     </div>
   );
 }
