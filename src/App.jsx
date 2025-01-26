3cc5fb64d68e5a2b6711ca58f318629fc1ca61ba
ayoncoding404 <muhayon.rxz@gmail.com>
2025-01-27 02:43:29 +0600

    pass state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-
-  const[length, setLength]=useState(8);
-  const[numAllowed, setNumAllowed] =useState(false);
-  const [char, setChar] =useState(false);
-  
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [char, setChar] = useState(false);
+  const [pass, setPass] = useState("");
   return (
-    <div >
-    <h1 className="text-white text-4xl text-center">Password Generator</h1>
+    <div>
+      <h1 className="text-white text-4xl text-center">Password Generator</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
